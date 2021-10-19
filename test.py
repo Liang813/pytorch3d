21e2f0cbfc4a6def857a3b2d14cea5e842488937
lyk <62755766+Liang813@users.noreply.github.com>
2021-10-19 10:33:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 
-for i in range(5):
-    print(mesh_laplacian_smoothing(ico_sphere(i), 'cotcurv'))
+from pytorch3d.loss import mesh_laplacian_smoothing
+from pytorch3d.utils import ico_sphere
+
+if __name__ == '__main__':
+    for i in range(5):
+        print(mesh_laplacian_smoothing(ico_sphere(i), 'cotcurv'))
 
 # Output:
 # tensor(0.1652)
@@ -8,3 +12,4 @@ for i in range(5):
 # tensor(6.1977)
 # tensor(25.4255)
 # tensor(102.4149)
+
