fcb3bd482beea053f83857a0950c4198e512a0d1
lyk <62755766+Liang813@users.noreply.github.com>
2021-10-19 10:26:03 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+
+for i in range(5):
+    print(mesh_laplacian_smoothing(ico_sphere(i), 'cotcurv'))
+
+# Output:
+# tensor(0.1652)
+# tensor(1.3907)
+# tensor(6.1977)
+# tensor(25.4255)
+# tensor(102.4149)
